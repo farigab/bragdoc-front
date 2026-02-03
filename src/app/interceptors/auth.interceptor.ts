2fe64749bbcbd3e8c5ef23dd1ddd7bf3a3f0a0e3
Gabriel F <f-gab@outlook.com>
2026-02-03 19:09:35 -0300

    fix: refresh token

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,22 +1,9 @@
-import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
-import { Router } from '@angular/router';
+import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = inject(Router);
-
   const clonedReq = req.clone({
     withCredentials: true
   });
 
-  return next(clonedReq).pipe(
-    catchError((error: HttpErrorResponse) => {
-      if (error.status === 401) {
-        router.navigate(['/login']);
-      }
-
-      return throwError(() => error);
-    })
-  );
+  return next(clonedReq);
 };
